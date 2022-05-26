9ea4c0a1cac0f82933d25a41625aac15edda7c6e
dierickalonso <102127799+dierickalonso@users.noreply.github.com>
2022-05-25 21:32:02 -0500

    Add files via upload

---
+++ DSW_II_Project_Backend/src/main/java/com/empresa/DSW_II_Project_Backend.java
@@ -0,0 +1,13 @@
+package com.empresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSW_II_Project_Backend {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DSW_II_Project_Backend.class, args);
+	}
+
+}
